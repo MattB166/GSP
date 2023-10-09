23bb7fef7162616dfb5cea211b059f975285f653
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-10-09 15:08:18 +0100

    updated axe throw

--- MatthewBrakeGSPDemo1/Assets/Scripts/Axe Throw.cs
+++ MatthewBrakeGSPDemo1/Assets/Scripts/Axe Throw.cs
@@ -12,22 +12,47 @@ public class AxeThrow : MonoBehaviour
     Vector3 currentEulerAngles;
     public float z;
     private Rigidbody2D rb;
+    private Vector2 initialPosition;
+    public float maxDistance = 10f;
 
-    private Vector2 direction; 
+    private Vector2 direction;
+
+    private bool isReturning = false;
     
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position;
         
     }
 
     private void FixedUpdate()
     {
+       if(!isReturning)
+        {
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            rb.velocity = -direction * speed;
+        }
+        
+        
+        
         rb.velocity = direction * speed;
         currentEulerAngles += new Vector3(0,0,z) * Time.deltaTime * rotationSpeed;
         transform.localEulerAngles = currentEulerAngles; 
+
+        float currentDistance = Vector2.Distance(initialPosition, transform.position);
+
+        if(currentDistance >= maxDistance)
+        {
+            direction = -direction;
+
+            isReturning = !isReturning;
+        }
        
     }
 
