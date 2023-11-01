291aa691a8eed0801a16afa00e78fd2e9d53ef97
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-11-01 18:36:30 +0000

    tweaks. cant put document away yet

--- Matthew Brake Tech Demo 2/Assets/DocumentDisplay.cs
+++ Matthew Brake Tech Demo 2/Assets/DocumentDisplay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 
@@ -23,6 +24,7 @@ public class DocumentDisplay : MonoBehaviour
         {
             Debug.LogError("Document text is not assigned in inspector");
         }
+       
         
         
        
