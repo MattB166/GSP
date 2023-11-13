59fab1387f4635111f8280b435a91e26e0035931
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-11-13 12:37:25 +0000

    can tap enemy and "select as target" even when moving

--- MatthewBrakeTechDemo3/Assets/Scripts/Enemy/EnemyController.cs
+++ MatthewBrakeTechDemo3/Assets/Scripts/Enemy/EnemyController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
    public EnemyStats EnemyStats;
+   public TextMeshProUGUI text; //for testing 
+   ////need reference to UI so can change current target  
     
     
     // Start is called before the first frame update
@@ -16,6 +19,36 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       if(Input.touchCount==1  || Input.touchCount==2)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began)
+            {
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 touchPos = new Vector2(pos.x, pos.y);
+
+                var Hit = Physics2D.OverlapPoint(touchPos); 
+                if(Hit)
+                {
+                    if(Hit.transform == transform)
+                    {
+                        text.GetComponent<TextMeshProUGUI>().text = "Tapped. Target Selected"; 
+
+                        // change UI to reflect new target
+                        // "gameobject is current target or smth like that 
+                    }
+                }
+                else
+                {
+                    text.GetComponent<TextMeshProUGUI>().text = "Not tapped. no target selected";
+                }
+            }
+            else if(Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(1).phase == TouchPhase.Ended)
+            {
+                text.GetComponent<TextMeshProUGUI>().text = "End of tap";
+            }
+        }
+        
+        
         CheckforPlayerInRange(); 
     }
     void initialiseEnemy()
@@ -54,5 +87,14 @@ public class EnemyController : MonoBehaviour
         {
             Debug.Log("Player Out of range");
         }
+    }
+
+    private void OnMouseUp()
+    {
+       // text.GetComponent<TextMeshProUGUI>().text = "Released Tap!";    //testing can tap 
+    }
+    private void OnMouseDown()
+    {
+       //text.GetComponent<TextMeshProUGUI>().text = "Tapped!"; 
     }
 }
