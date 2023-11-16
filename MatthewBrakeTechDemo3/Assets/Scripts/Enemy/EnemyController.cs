9ef27527efd17e28eb18a85579a158578ad59e33
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-11-16 20:52:52 +0000

    enemy targeting fixed

--- MatthewBrakeTechDemo3/Assets/Scripts/Enemy/EnemyController.cs
+++ MatthewBrakeTechDemo3/Assets/Scripts/Enemy/EnemyController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.EnhancedTouch;
 
 public class EnemyController : MonoBehaviour
@@ -26,48 +28,59 @@ public class EnemyController : MonoBehaviour
     {
       
         
-        if(Input.touchCount >= 0)
+        if(Input.touchCount > 0)
         {
             if (Input.GetTouch(0).phase == TouchPhase.Began)
+            { 
+                    if(EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
                 {
-                    Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    return;
+                }
+                
+                
+                
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     Vector2 touchPos = new Vector2(pos.x, pos.y);
 
-                    var Hit = Physics2D.OverlapPoint(touchPos);
-                    if (Hit)
+                int layermask = LayerMask.GetMask("Enemy", "Ground");
+
+                RaycastHit2D Hit = Physics2D.Raycast(touchPos, Vector2.zero, Mathf.Infinity, layermask);
+
+                Debug.Log("Hit Collider tag: " + Hit.collider.tag);
+
+                   // var Hit = Physics2D.OverlapPoint(touchPos);
+                if (Hit.collider != null)
+                {
+                    if(Hit.collider.CompareTag("Enemy"))
                     {
-                       EnemyController newTarget = Hit.transform.GetComponent<EnemyController>();
-                    if (newTarget != null)
+                        text.GetComponent<TextMeshProUGUI>().text = "Tapped. Target selected";
+                        GameManager.instance.SetActiveEnemy(Hit.collider.GetComponent<EnemyController>());
+                        currentActiveEnemy = Hit.collider.GetComponent<EnemyController>();
+                    }
+                    else if(Hit.collider.CompareTag("Ground"))
                     {
-
-                        if (Hit.transform == transform)
-                        {
-
-
-                            text.GetComponent<TextMeshProUGUI>().text = "Tapped. Target Selected";
-                            GameManager.instance.SetActiveEnemy(newTarget);
-                            currentActiveEnemy = newTarget;
-                            
-
-                            // change UI to reflect new target
-                            // "gameobject is current target or smth like that 
-                        }
-
+                        text.GetComponent<TextMeshProUGUI>().text = "Tapped On ground. Target Deselected";
+                        GameManager.instance.SetActiveEnemy(null);
+                        currentActiveEnemy = null; 
                     }
-                    else
+                   else
                     {
-                         //GameManager.instance.SetActiveEnemy(null);
+                        //GameManager.instance.SetActiveEnemy(null);
                     }
-                    }
-                    else
-                    {
-                        text.GetComponent<TextMeshProUGUI>().text = "Not tapped. no target selected";
-                       // GameManager.instance.SetActiveEnemy(null);
-                    }
+                }
+                else
+                {
+                    text.GetComponent<TextMeshProUGUI>().text = "Not tapped. no target selected";
+                    // GameManager.instance.SetActiveEnemy(null);
+                }
                 }
                 else if (Input.GetTouch(0).phase == TouchPhase.Ended)
                 {
                     text.GetComponent<TextMeshProUGUI>().text = "End of tap";
+                    
+                   
+
+                    
                 }
             
 
